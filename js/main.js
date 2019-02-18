157698040cc55ebbc6ed27139e2c0ace7141d1b1
Sam Poate <sampoate@outlook.com>
2019-02-18 15:17:42 +0000

    Added slick image scroller to sponsors

--- js/main.js
+++ js/main.js
@@ -16,6 +16,19 @@ $(document).on("scroll", function(e) {
 // Deletes last menu item on mobile
 if (window.outerWidth <= 768) {
   $("#navbar ul li:last-child").remove();
+  $(".autoplay").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+} else {
+  $(".autoplay").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
 }
 
 //Smooth Srolling
@@ -49,24 +62,26 @@ $("#navbar a").on("click", function(event) {
 //   }
 // });
 
-$({countNum: 99}).animate({countNum: 1000}, {
-  duration: 8000,
-  easing:'linear',
-  step: function() {
-    // What todo on every count
-    console.log(Math.floor(this.countNum));
-  },
-  complete: function() {
-    console.log('finished');
+$({ countNum: 99 }).animate(
+  { countNum: 1000 },
+  {
+    duration: 8000,
+    easing: "linear",
+    step: function() {
+      // What todo on every count
+      console.log(Math.floor(this.countNum));
+    },
+    complete: function() {
+      console.log("finished");
+    }
   }
-});
+);
 
 // Set the date we're counting down to
 var countDownDate = new Date("Dec 12, 2020 15:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
-
   // Get todays date and time
   var now = new Date().getTime();
 
@@ -80,8 +95,8 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("counter").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("counter").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
